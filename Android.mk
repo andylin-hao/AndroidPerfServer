ca19e5ed651e5dc92a1a3e9e588c9a160a2df6c7
andylin-hao <linhao-16@outlook.com>
2022-10-06 01:10:47 +0800

    use mk to make oreo happy

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_MODULE := AndroidPerfServerFWlib
 LOCAL_MODULE_STEM := AndroidPerfServerFW
+LOCAL_REQUIRED_MODULES := AndroidPerfServer
 include $(BUILD_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
